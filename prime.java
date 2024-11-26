6c4bfe02dd64a233854ecf17d98f613f2aa63246
Niha3579 <22h51a05k0@cmrcet.ac.in>
2024-11-26 22:20:50 +0530

    2 commit

--- prime.java
+++ prime.java
@@ -1,6 +1,6 @@
 public class Prime{
     public static void main(String args[]){
-        int num=3;
+        int num=30;
         boolean isPrime=true;
         for(int i=2;i<num;i++){
             if(num%i==0){
